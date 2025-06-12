f81ced1faaa545ac4ed26c0aa0d179f7ea5ba181
mohammed-hakim <devhakimpro88@gmail.com>
2025-06-12 23:27:47 +0100

    up vercel conf

---
+++ git.bash
@@ -0,0 +1,7 @@
+# echo "# EURL BHT" >> README.md
+# git init
+git add .
+git commit -m "up vercel conf"
+# git branch -M main
+# git remote add origin https://github.com/mohammed-hakim/bht.git
+git push -u origin main
